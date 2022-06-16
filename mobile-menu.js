6c1c92639de23f912bc7797af18e2aa723a87e37
NOkuneva <NOkuneva@slb.com>
2022-06-17 01:02:50 +0500

    feat: add mobile menu

---
+++ mobile-menu.js
@@ -0,0 +1,38 @@
+const menuControls = document.querySelectorAll(".mona-lisa-section__mobile-burger-menu img");
+const burgerMenuIcon = document.querySelector("img[alt='burger-menu']");
+const closeMenuIcon = document.querySelector("img[alt='close-menu']");
+
+const monaLisaText = document.querySelector(".mona-lisa-section__content-text");
+const mobileMenu = document.querySelector(".mobile-menu");
+const navigationLinksArray = document.querySelectorAll(".mobile-menu__navigation-group a");
+
+burgerMenuIcon.addEventListener("click", openMenu);
+closeMenuIcon.addEventListener("click", closeMenu);
+
+function openMenu() {
+    menuControls.forEach((menuIcon) => menuIcon.classList.toggle("not-active"));
+
+    if (window.innerWidth >= 821) {
+        mobileMenu.style.width = "250px";
+    } else {
+        mobileMenu.style.width = "100%";
+    }
+
+    monaLisaText.style.opacity = "0";
+    navigationLinksArray.forEach(link => link.addEventListener("click", closeMenu));
+}
+
+function closeMenu() {
+    menuControls.forEach((menuIcon) => menuIcon.classList.toggle("not-active"));
+    mobileMenu.style.width = "0";
+    monaLisaText.style.opacity = "1";
+    navigationLinksArray.forEach(link => link.removeEventListener("click", closeMenu));
+}
+
+window.onresize = () => {
+
+    if (burgerMenuIcon.classList.length) {
+        closeMenu();
+    }
+}
+
